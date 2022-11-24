cc3615eaff7ed0dc7735432206d212f3fb72a4be
Scott F <98131933+SF-11@users.noreply.github.com>
2022-11-24 00:49:40 -0500

    added external barcode lookup api functionality

--- src/main/java/sf/inventory/barcode/BarcodeService.java
+++ src/main/java/sf/inventory/barcode/BarcodeService.java
@@ -1,5 +1,7 @@
 package sf.inventory.barcode;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,22 +12,28 @@ import sf.inventory.external.BarcodeAPI;
 public class BarcodeService {
 
     @Autowired
-    private BarcodeAPI barcodeApi;
+    private BarcodeAPI barcodeAPI;
+
+    @Autowired
+    private static Logger logger = LoggerFactory.getLogger(BarcodeService.class);
+
 
     public ResponseEntity<String> postBarcode(String barcode) {
         // validate barcode
         if (!validateBarcode(barcode)) {
+            logger.info("Invalid barcode: " + barcode);
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid barcode: " + barcode);
         }
 
         // lookup barcode
+        String item = barcodeAPI.lookupBarcode(barcode);
 
 
         // post result to sheets
-
+        // sheetsAPI.postValues();
 
         // return response
-        return ResponseEntity.ok(barcode);
+        return ResponseEntity.ok(item);
     }
 
 
--- src/main/java/sf/inventory/external/BarcodeAPI.java
+++ src/main/java/sf/inventory/external/BarcodeAPI.java
@@ -1,20 +1,113 @@
 package sf.inventory.external;
 
-import org.springframework.boot.json.BasicJsonParser;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
+import java.io.IOException;
 import java.util.Map;
 
 @Component
 public class BarcodeAPI {
 
+    @Value("${barcode.api.url}")
+    private String url;
 
-    public Map<String, String> lookupBarcode(String barcode) {
-        String response = "";
+    private static Logger logger = LoggerFactory.getLogger(BarcodeAPI.class);
 
-        BasicJsonParser parser = new BasicJsonParser();
-        parser.parseMap(response);
-        return null;
+    /**
+     * Get the title of the item corresponding to the barcode
+     * uses the upcitemdb API
+     *
+     * @param barcode
+     * @return
+     */
+    public String lookupBarcode(String barcode) {
+
+        Response response = apiSearch(barcode);
+        if (response.code() == 200) {
+            logger.info("Barcode lookup request succeeded");
+        } else {
+            logger.info("Barcode lookup request failed with response code: " + response.code());
+            return "";
+        }
+
+        String title = "";
+        try {
+            title = parseJson(response.body().string());
+        } catch (IOException e) {
+            // TODO
+        }
+
+        if (StringUtils.hasLength(title)) {
+            logger.info("Title found in barcode response: " + title);
+        }
+
+        return title;
     }
 
+
+    /**
+     * TODO
+     * @param barcode
+     * @return
+     */
+    private Response apiSearch(String barcode) {
+        OkHttpClient client = new OkHttpClient();
+
+        HttpUrl httpUrl = HttpUrl.parse(url).newBuilder()
+                .addQueryParameter("upc", barcode)
+                .build();
+
+        Request request = new Request.Builder()
+                .url(httpUrl)
+                .build();
+
+        Response response = null;
+        try {
+            response = client.newCall(request).execute();
+        } catch (Exception e) {
+            logger.error("Barcode API request failed with request: " + request.url());
+        }
+
+        return response;
+    }
+
+
+    /**
+     * TODO
+     *
+     * @param json
+     * @return
+     */
+    private String parseJson(String json) {
+        JsonElement root = null;
+        String title = "";
+        try {
+            root = JsonParser.parseString(json);
+        } catch (Exception e) {
+            logger.error("Failed to parse Barcode API response: " + json);
+        }
+
+        try {
+            title = root.getAsJsonObject()
+                    .get("items")
+                    .getAsJsonArray().get(0)
+                    .getAsJsonObject()
+                    .get("title")
+                    .getAsString();
+        } catch (Exception e) {
+            logger.error("Failed to find title in JSON response: " + root.getAsString());
+        }
+
+        return title;
+    }
 }
