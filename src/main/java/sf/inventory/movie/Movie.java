a1dd7f6182d61bf37fdada791c2e827a7894a814
Scott F <98131933+SF-11@users.noreply.github.com>
2023-01-22 19:51:42 -0500

    Made db persistent

--- src/main/java/sf/inventory/movie/Movie.java
+++ src/main/java/sf/inventory/movie/Movie.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 public class Movie {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     public long getId() {
